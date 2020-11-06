7a78e71082371f52b02a472777640e2ec3c866be
it63070003 <73647989+it63070003@users.noreply.github.com>
2020-11-06 18:43:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Zomblox</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container" style="background-color: bisque;">
+        <div class="row">
+            <div class="col-lg-4 mt-3 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="img/blox1.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title">Project Lazarus: 💀 ZOMBIES 💀</h5>
+                        <p class="card-text">Survive the endless waves of zombie hordes in this cooperative shooter. In Project Lazarus: Zombies, you earn points by killing zombies and repairing barricades. Get better weapons and open up new areas before the deadly zombies overrun you. and dead in the grave</p>
+                        <a href="https://www.roblox.com/games/443406476/Project-Lazarus-ZOMBIES" class="btn btn-primary">Play Now</a>
+                    </div>
+                    <div class="card-footer">
+                        <img src="img/own1.png" class="image-fluid rounded-circle" style="width: 15%;"></img>
+                        By PinkzMan
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mt-3 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="img/zomb2.png">
+                    <div class="card-body" style="height: 100%;">
+                        <h5 class="card-title">🎃 Zombie Rush</h5>
+                        <p class="card-text">Fight off wave after wave of zombies and unlock new weapons as you progress through levels, or play as a zombie to finish off the remaining survivors! Aim for the head to make quick work of zombies! and destroy their entries base wow this game is very nice play now</p>
+                        <a href="https://www.roblox.com/games/137885680/Zombie-Rush" class="btn btn-primary">Play Now</a>
+                    </div>
+                    <div class="card-footer">
+                        <img src="img/own2.png" class="image-fluid rounded-circle" style="width: 15%;"></img>
+                        By WhithyeGirlza
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mt-3 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="img/blox2.jpg">
+                    <div class="card-body">
+                        <h5 class="card-title">Zombie Uprising</h5>
+                        <p class="card-text">Survive against hordes of zombies! It's up to you to save mankind!
+                            Available on computer, mobile, tablet and Xbox One! Earn EXP in game until November 9th to earn free skins and up to +20% money/points/EXP!
+                            Use the 'Event' button at the top of the screen to find out more.</p>
+                            <a href="https://www.roblox.com/games/4972091010/EVENT-Zombie-Uprising" class="btn btn-primary">Play Now</a>
+                    </div>
+                    <div class="card-footer">
+                        <img src="img/own3.png" class="image-fluid rounded-circle" style="width: 15%;"></img>
+                        By FutureDudez
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
